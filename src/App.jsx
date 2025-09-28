18875cc9a80393f37fdd6d2d192682d9d711dc5d
susmitha530 < GitHub email>
2025-09-28 14:49:58 +0530

    update: update job-section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer/Footer";      
+import JobsSection from "./pages/job/JobSection";
 import "./App.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/jobs" element={<JobsSection />} />
         <Route path="/footer" element={<Footer />} />
       </Routes>
     </Router>
