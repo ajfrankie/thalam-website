ab047d5c7637d3c449bd046dfd64481fc3bc0a0e
codeorbitsathushana <codeorbit.sathushana@gmail.com>
2025-09-28 19:14:23 +0530

    update:update testominal

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Footer from "./components/Footer";      
-import JobsSection from "./pages/job/JobSection";
-import "./App.css";
-import "@fortawesome/fontawesome-free/css/all.min.css";
+
+
+import Home from "./pages/home";
+
+import Testimonial from "./pages/home/testimonial";
+
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/jobs" element={<JobsSection />} />
-        <Route path="/footer" element={<Footer />} />
+        <Route path="/" element={<Home />} />
+        
+        <Route path="/testimonial" element={<Testimonial />} />
       </Routes>
     </Router>
   );
