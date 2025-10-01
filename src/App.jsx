8ed53d94ef62fd14e6d3a5c464f90a1357782f07
codeorbitsanshika-sashikumar <codeorbit.sanshika@gmail.com>
2025-10-01 09:50:03 +0530

    update:update homne

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer";
 import Navbar from "./components/navbar";
 import "@fortawesome/fontawesome-free/css/all.min.css";
-
+import Home from "./pages/home/home";
 function App() {
   return (
     <Router>
       <Routes>
         
           <Route path="navbar" element={<Navbar />} />
+          <Route path="home" element={<Home />} />
         
        
         <Route path="/Footer" element={<Footer />} />
