be0d421977d27ea39c18b90b64227fb623daab55
codeorbitsanshika-sashikumar <codeorbit.sanshika@gmail.com>
2025-10-01 09:50:03 +0530

    update:update homne

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,22 @@ import Footer from "./components/Footer";
 import JobsSection from "./pages/job/JobSection";
 import "./App.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
-
+import Home from "./pages/home/home";
 function App() {
   return (
     <Router>
       <Routes>
+<<<<<<< HEAD
         <Route path="/jobs" element={<JobsSection />} />
         <Route path="/footer" element={<Footer />} />
+=======
+        
+          <Route path="navbar" element={<Navbar />} />
+          <Route path="home" element={<Home />} />
+        
+       
+        <Route path="/Footer" element={<Footer />} />
+>>>>>>> 8ed53d9 (update:update homne)
       </Routes>
     </Router>
   );
