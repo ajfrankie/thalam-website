27771205b3381f9a08d277230449cd0e8cfb9df1
piriyavathani <codeorbit.priyavathani@gmail.com>
2025-10-03 11:09:21 +0530

    resolve merge conflict in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
-import Home from "./pages/home";
-import Testimonial from "./pages/home/testimonial";
-import OurServices from "./pages/history/ourServices";
-import History from "./pages/history/history";
+
+import Home from "./pages/home"; 
+import Testimonial from "./pages/home/testimonial"; 
+import OurServices from "./pages/history/ourServices"; 
+import History from "./pages/history/history"; 
 
 import ImpactCards from "./components/impact/ImpactCards";
-import EducationCoures from "./pages/education/EducationCoures";
+import EducationCoures from "./pages/education/EducationCoures"; 
+
+
+
+
 
 function App() {
   return (
@@ -18,41 +23,12 @@ function App() {
         <Route path="/ourServices" element={<OurServices />} />
         <Route path="/history" element={<History />} />
 
-        {/* Redirect + Other Components */}
-        <Route path="/" element={<Navigate to="/ImpactCards" />} /> {/* Default redirect */}
-        <Route path="/ImpactCards" element={<ImpactCards />} />
-        <Route path="/EducationCoures" element={<EducationCoures />} />
-      </Routes>
-    </Router>
-  );
-}
-
-export default App;
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-
-// Import all necessary components 
-import Home from "./pages/home/Home";
-import Testimonial from "./pages/home/testimonial/Testimonial";
-import OurServices from "./pages/history/ourservices/OurServices";
-
-// Import components for new routes (If these components exist in your project)
-// Note: If you know the correct paths for ImpactCards and EducationCourses, use them instead of the comments.
-// import ImpactCards from "./pages/impactcards/ImpactCards"; 
-// import EducationCourses from "./pages/educationcourses/EducationCourses"; 
-
-function App() {
-  return (
-    <Router>
-      <Routes>
-        {/* All routes merged here from both sides, without duplicates */}
-        <Route path="/testimonial" element={<Testimonial />} />
-        <Route path="/ourservices" element={<OurServices />} />
-        <Route path="/history" element={<History />} /> 
+        {/* Home page should ideally point to /ImpactCards, so using Navigate or ImpactCards here */}
+        <Route path="/" element={<Navigate to="/ImpactCards" />} /> 
         
-        <Route path="/" element={<Home />} /> 
         <Route path="/ImpactCards" element={<ImpactCards />} />
-        <Route path="/EducationCourses" element={<EducationCourses />} />
+        <Route path="/EducationCoures" element={<EducationCoures />} /> 
+        
       </Routes>
     </Router>
   );
