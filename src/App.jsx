67826d422075721189deb4eec960dfc0601f27c4
susmitha530 < GitHub email>
2025-10-05 14:08:54 +0530

    Removed unused images

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,11 @@ import Home from "./pages/home";
 import Testimonial from "./pages/home/testimonial";
 import OurServices from "./pages/history/ourServices";
 import History from "./pages/history/history";
-
 import ImpactCards from "./components/impact/ImpactCards";
 import EducationCources from "./pages/education/EducationCources";
-import JobsSection from "./components/jobs/JobsSection";   // <-- include this import
+import JobsSection from "./components/jobs/JobsSection";
+import CountdownTimer from "./components/countdown/CountdownTimer"; 
+import HomeTab from "./components/home/HomeTab"; 
 
 function App() {
   return (
@@ -17,10 +18,14 @@ function App() {
         {/* Main Home Page */}
         <Route path="/" element={<Home />} />
 
-        {/* Direct routes if needed */}
+        {/* Additional routes */}
         <Route path="/testimonial" element={<Testimonial />} />
         <Route path="/countdowntimer" element={<CountdownTimer />} />
         <Route path="/hometab" element={<HomeTab />} />
+        <Route path="/jobs" element={<JobsSection />} /> {/* ✅ Added this */}
+        <Route path="/ourservices" element={<OurServices />} />
+        <Route path="/history" element={<History />} />
+        <Route path="/education" element={<EducationCources />} />
       </Routes>
     </Router>
   );
