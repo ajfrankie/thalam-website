9b42f0203f2078138866e26cbd3c3cf4c2bfa917
susmitha530 < GitHub email>
2025-10-05 14:28:07 +0530

    update job

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/home";
-import Testimonial from "./pages/home/testimonial";//testimonial
+import Testimonial from "./pages/home/testimonial";
 import OurServices from "./pages/history/ourServices";
 import History from "./pages/history/history";
 import ImpactCards from "./components/impact/ImpactCards";
