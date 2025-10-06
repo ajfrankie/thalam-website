dd82dbbddf16db037e3817d6351e279664122eba
Your Name <your_email@example.com>
2025-10-06 14:31:50 +0530

    Updated App.jsx before merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-<<<<<<< HEAD
 
 import Home from "./pages/home";
-import Testimonial from "./pages/home/testimonial";
+import Testimonial from "./pages/home/Testimonial";
 import OurServices from "./pages/history/ourServices";
 import History from "./pages/history/history";
-=======
+
 // Home will auto-load from pages/home/index.jsx
 import Home from "./pages/home";  
->>>>>>> b1f6279adf01e7a4999cabb82d211531b40e856d
+
 
 // sub components for direct routes
 import Testimonial from "./pages/home/Testimonial";
@@ -21,13 +20,13 @@ function App() {
   return (
     <Router>
       <Routes>
-<<<<<<< HEAD
+
         {/* Home page */}
         <Route path="/home" element={<Home />} />
-=======
+
         {/* Main Home Page */}
         <Route path="/" element={<Home />} />
->>>>>>> b1f6279adf01e7a4999cabb82d211531b40e856d
+
 
         {/* Direct routes if needed */}
         <Route path="/testimonial" element={<Testimonial />} />
