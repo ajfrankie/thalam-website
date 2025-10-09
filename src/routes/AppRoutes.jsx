83e92f6339dc019df35dd76cf334c515f1605cdd
Your Name <your_email@example.com>
2025-10-09 12:20:17 +0530

    Education update

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -14,7 +14,7 @@ function App() {
         <Route path="/impact" element={<ImpactCards />} />
       </Routes>
 
-      {/* Include extra route set */}
+      {/* Include extra  */}
       <AppRoutes />
     </Router>
   );
