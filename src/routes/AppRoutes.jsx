924c15eedb67bb34a551ad9a74fb8a27205a2f33
Your Name <your_email@example.com>
2025-10-09 12:16:12 +0530

    update Education

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,60 +1,23 @@
 import React from "react";
-
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import EducationCoures from "./pages/education/EducationCoures";
-import ImpactCards from "./components/impact/ImpactCards";
+import EducationCoures from "../pages/education/EducationCoures";
+import ImpactCards from "../components/impact/ImpactCards";
+import AppRoutes from "../routes/AppRoutes"; // separate file import
 
 function App() {
   return (
     <Router>
       <Routes>
         {/* Default redirect */}
-        <Route path="/" element={<Navigate to="/impact" replace />} />//code
-        
-        {/* Routes */}
+        <Route path="/" element={<Navigate to="/impact" replace />} />
         <Route path="/education" element={<EducationCoures />} />
         <Route path="/impact" element={<ImpactCards />} />
-
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "@fortawesome/fontawesome-free/css/all.min.css";
-
-
-import Home from "../pages/Home";       
-import Education from "../pages/Education";
-import History from "../pages/history";
-
-
-
-
-
-
-
-
-function AppRoutes() {
-  return (
-    <Router>
-      <Routes>
-       
-        <Route path="/Home" element={<Home />} />
-        <Route path="/Education" element={<Education />} />
-         <Route path="/history" element={<History />} />
-       
-       
-        
-        
-
-
-        
-
       </Routes>
+
+      {/* Include extra route set */}
+      <AppRoutes />
     </Router>
   );
 }
 
-
 export default App;
-
-export default AppRoutes;
-
-/* 🔹 Added Syne font */
-
