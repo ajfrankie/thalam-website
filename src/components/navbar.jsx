0cd82d0846cb1d3801050da611bad543f9efca6d
codeorbitsathushana <codeorbit.sathushana@gmail.com>
2025-10-09 21:46:11 +0530

    update navbar history

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import logo from "../assets/images/logo.png";   // 👈 correct import (src/assets/logo.png irundha)
+import logo from "../assets/images/logo.png";   
 
 
 
