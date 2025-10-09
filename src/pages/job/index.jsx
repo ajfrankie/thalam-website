4b422b2ee60ceef1f3a322970ef8bbb2ea55b41e
susmitha530 < GitHub email>
2025-10-09 14:06:58 +0530

    update jobs

--- src/pages/job/index.jsx
+++ src/pages/job/index.jsx
@@ -1,16 +1,22 @@
 // src/pages/home/index.jsx
 import React from "react";
+
+// Correct imports (check filenames are exact)
 import Navbar from "../../components/navbar";
 import JobSection from "../../pages/job/JobSection";
-import Footer from "../components/Footer/Footer";
+
+import Footer from "../components/Footer/Footer"; // Footer.jsx
 
 const Home = () => {
   return (
-    <main>
-      <Navbar />
-      <JobSection />
+    <>
+      {/* Render all components in one page */}
+      <Navbar/>
+      <JobSection/>
       <Footer />
-    </main>
+
+
+    </>
   );
 };
 
