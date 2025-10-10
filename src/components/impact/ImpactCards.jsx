a4c4e7db87803abd9f03d5da048e7fe164fa4def
Your Name <your_email@example.com>
2025-10-10 14:52:13 +0530

    update impact

--- src/components/impact/ImpactCards.jsx
+++ src/components/impact/ImpactCards.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import "./ImpactCards.css";
 import impactcards1 from "../../assets/images/impactcards1.jpg";
-import impactcards2 from "../../assets/images/impactcards2.jpg";
+import impactcards2 from "../../assets/images/impactcards2.jpg";//image
 import impactcards3 from "../../assets/images/impactcards3.jpg";//image
 import impactcards4 from "../../assets/images/impactcards4.jpg";//image
 
