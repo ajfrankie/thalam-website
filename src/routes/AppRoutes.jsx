ef2af3320352bda407d3983cf9dbf5f61d62100d
codeorbitsathushana <codeorbit.sathushana@gmail.com>
2025-10-10 12:48:19 +0530

    update Approutes

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
-// Home (index.jsx) matthum import pannunga
+//Home index
 import Home from "../pages/home";       
 import Education from "../pages/Education";
 import History from "../pages/history";
