1070a647f72e830c5356763a14a0b2d5ef769b0e
susmitha530 < GitHub email>
2025-10-10 20:32:33 +0530

    Updated AppRoutes locally

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -2,26 +2,25 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
-//Home index
-import Home from "../pages/home";       
+// Home index
+import Home from "../pages/home";
 import Education from "../pages/Education";
 import History from "../pages/history";
-import Job from "../pages/Job";
+import Job from "../pages/job";
+import Register from "../pages/auth/register"; // ✅ fixed path
 
 function AppRoutes() {
   return (
     <Router>
       <Routes>
-       
         <Route path="/Home" element={<Home />} />
         <Route path="/Education" element={<Education />} />
         <Route path="/history" element={<History />} />
-        {/* <Route path="/Job" element={<Job />} /> */}
         <Route path="/Job" element={<Job />} />
-        
-        </Routes>
+        <Route path="/register" element={<Register />} /> {/* ✅ working now */}
+      </Routes>
     </Router>
   );
 }
 
-export default AppRoutes; 
+export default AppRoutes;
