cc9d77dc14f8d923f0be70bcc4bcef9e1b931fdd
codeorbitsathushana <codeorbit.sathushana@gmail.com>
2025-10-13 10:25:28 +0530

    update home

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,16 +1,20 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import Home from "../pages/home";
 import Register from "../pages/auth"; 
 import Contact from "../pages/contact-us/contact";
-
+import Job from "../pages/Job";
+import Footer from "../components/Footer/Footer";
 function AppRoutes() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/Home" element={<Home />} />
         {/* ✅ இந்த line சேர்க்கணும் */}
         <Route path="/register" element={<Register />} />
         <Route path="/contact" element={<Contact />} />
+         <Route path="/Job" element={<Job />} />
+         <Route path= "/Footer" element={<Footer/>}/>
       </Routes>
     </Router>
   );
